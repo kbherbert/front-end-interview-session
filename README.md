ae7848f5ff340737584731e3a9b4f95dbef1ae55
Brady Herbert <brady@bradyh.com>
2014-11-03 22:07:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+##Working session 
+*version 1.1, last Updated: 11/02/14*
+
+----------------
+
+In this live programming example, the interviewee will be asked to complete the following, given the markup and CSS in this directory.
+
+1.	The call to action in the text widget needs some visual improvements. Update it with the following UX requirements:
+	*	The "Learn more" call-to-action should be styled like a button:
+		*	It should have 10px rounded corners
+		*	The button will be a solid green color (#1d5b2d)
+		*	The button text should be white with a size of 16px
+	*	On hover, the button should change:
+		*	To a darker shade of green (#103e1b)
+
+2.	The demo page is not responsive. Make it responsive with the following:
+	*	Your breakpoints are:
+		*	Phones: 320px - 767px
+		*	Tablets: 768px - 979px
+	*	The two widgets should stack on phones, but remain side-by-side on tablets
+	*	The two columns of links in the link widget should collapse into one column on both phones and tablets
+
+3.	Add some interactivity to the links in the link widget
+	*	Use jQuery (provided) to add efficient click event listeners for each link
+	*	Stop the default behavior from happening, and instead send an ajax request to the resource contained in each links `href` attribute
+	*	You'll get a response to your ajax request that contains information about each resource. Without knowing the data contract, inspect the response and show the user the user-friendly message contained in each response.
---
+++ js/routes.js
@@ -0,0 +1,87 @@
+$.mockjax({
+	url: '/resources/links/1',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 1,
+		resourceName: 'Google',
+		resourceDescription: "Google is an American multinational corporation specializing in Internet-related services and products. These include online advertising technologies, search, cloud computing, and software. Most of its profits are derived from AdWords."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/2',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 2,
+		resourceName: 'Apple',
+		resourceDescription: "Apple Inc. is an American multinational corporation headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software and personal computers. Its best-known hardware products are the Mac line of computers, the iPod media player, the iPhone smartphone, and the iPad tablet computer. Its consumer software includes the OS X and iOS operating systems, the iTunes media browser, the Safari web browser, and the iLife and iWork creativity and productivity suites."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/3',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 3,
+		resourceName: 'Microsoft',
+		resourceDescription: "Microsoft Corporation is an American multinational corporation headquartered in Redmond, Washington, that develops, manufactures, licenses, supports and sells computer software, consumer electronics and personal computers and services. Its best known software products are the Microsoft Windows line of operating systems, Microsoft Office office suite, and Internet Explorer web browser. Its flagship hardware products are Xbox game console and the Microsoft Surface series of tablets. It is the world's largest software maker measured by revenues. It is also one of the world's most valuable companies."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/4',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 4,
+		resourceName: 'Facebook',
+		resourceDescription: "Facebook is an online social networking service. Its name comes from a colloquialism for the directory given to students at some American universities. Facebook was founded on February 4, 2004 by Mark Zuckerberg with his college roommates and fellow Harvard University students Eduardo Saverin, Andrew McCollum, Dustin Moskovitz and Chris Hughes."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/5',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 5,
+		resourceName: 'Twitter',
+		resourceDescription: "Twitter is an online social networking and microblogging service that enables users to send and read short 140-character text messages, called 'tweets'. Registered users can read and post tweets, but unregistered users can only read them. Users access Twitter through the website interface, SMS, or mobile device app. Twitter Inc. is based in San Francisco and has offices in New York City, Boston, San Antonio and Detroit."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/6',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 6,
+		resourceName: 'Yahoo',
+		resourceDescription: "Yahoo! Inc. is an American multinational Internet corporation headquartered in Sunnyvale, California. It is globally known for its Web portal, search engine Yahoo Search, and related services, including Yahoo Directory, Yahoo Mail, Yahoo News, Yahoo Finance, Yahoo Groups, Yahoo Answers, advertising, online mapping, video sharing, fantasy sports and its social media website."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/7',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 7,
+		resourceName: 'Amazon',
+		resourceDescription: "Amazon.com, Inc. is an American international electronic commerce company with headquarters in Seattle, Washington, United States. It is the world's largest online retailer. Amazon.com started as an online bookstore, but soon diversified, selling DVDs, VHSs, CDs, video and MP3 downloads/streaming, software, video games, electronics, apparel, furniture, food, toys, and jewelry. The company also produces consumer electronics—notably the Amazon Kindle e-book reader and the Kindle Fire tablet computer—and is a major provider of cloud computing services."
+	}
+});
+
+$.mockjax({
+	url: '/resources/links/8',
+	status: 200,
+	type: 'GET',
+	responseText: {
+		resourceId: 8,
+		resourceName: 'Oracle',
+		resourceDescription: "Oracle Corporation is a U.S. based multinational computer technology corporation headquartered in Redwood City, California, United States. The company specializes in developing and marketing computer hardware systems and enterprise software products – particularly its own brands of database management systems. Oracle is the second-largest software maker by revenue, after Microsoft."
+	}
+});
